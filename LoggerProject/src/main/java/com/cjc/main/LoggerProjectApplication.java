359b0b837491d9b7f7ddf3753df945c2e097d406
HP <vishalmorey1998@gmail.com>
2024-11-27 15:21:53 +0530

    demo

---
+++ LoggerProject/src/main/java/com/cjc/main/LoggerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggerProjectApplication.class, args);
+	}
+
+}
---
+++ LoggerProject/src/main/java/com/cjc/main/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.cjc.main.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController 
+{
+	private static final Logger log =LoggerFactory.getLogger(HomeController.class);
+	
+	@GetMapping("/")
+	public String defaultRequest()
+	{
+		log.error("this is erroe Event");
+		log.warn("this is warn Event");
+		log.info("this is info Event");
+		log.debug("this is debug Event");
+		return "check the Console";
+	}
+}
